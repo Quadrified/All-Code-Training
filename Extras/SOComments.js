3d8932caa34b7933603fa1729f7f6cca0bf7ed60
Omer Quadri M <omar@DSmart.com>
2020-01-09 20:43:04 +0530

    Added FB login

--- Extras/SOComments.js
+++ Extras/SOComments.js
@@ -10,4 +10,46 @@ GooglelogOut = async () => {
     console.error(error);
   }
 };
+
+renderLogoutButton() {
+    return (
+      <View style={{flex: 1}}>
+        <Button
+          title="Logout"
+          buttonStyle={{width: '90%', height: 60, margin: 20}}
+          onPress={this.logOut}
+        />
+      </View>
+    );
+  }
 //
+
+// FB Login
+
+<View>{this.FBAuth()}</View>
+FBAuth() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignContent: 'center',
+          marginLeft: 120,
+          marginTop: 30,
+        }}>
+        <LoginButton
+          onLoginFinished={(error, result) => {
+            if (error) {
+              console.log('login has error: ' + result.error);
+            } else if (result.isCancelled) {
+              console.log('login is cancelled.');
+            } else {
+              AccessToken.getCurrentAccessToken().then(data => {
+                console.log(data.accessToken.toString());
+              });
+            }
+          }}
+          onLogoutFinished={() => console.log('logout.')}
+        />
+      </View>
+    );
+  }
