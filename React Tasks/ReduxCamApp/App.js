0875b6ed552370d671fb9c760f07f8cbe7af96f6
Omer Quadri M <omar@DSmart.com>
2020-01-28 14:52:55 +0530

    Added Instructions

--- React Tasks/ReduxCamApp/App.js
+++ React Tasks/ReduxCamApp/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import ConverterDemo from './src/ConverterDemo';
 import Cropper from './src/CroppedConverter';
-import NewLib from './src/NewPDFlib';
-
+/**
+ *Use ConverterDemo.js for Image to PDF converter
+ *Use Cropped Converter for only image Cropping
+ */
 class App extends Component {
   render() {
     return <ConverterDemo />;
-    // return <Cropper />;
-    // return <NewLib />;
   }
 }
 export default App;
