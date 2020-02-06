0969988891df819e5844103d7b134236815d9c36
Omer Quadri M <omar@DSmart.com>
2020-02-06 19:32:07 +0530

    Added Token Decoder

--- React Tasks/FlatListApp/App.js
+++ React Tasks/FlatListApp/App.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react'
-import { Text, View, Button, StyleSheet } from 'react-native'
-import { createStackNavigator } from 'react-navigation';
+import React, {Component} from 'react';
+import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
+import JwtDecode from 'jwt-decode';
 
-class HomeApp extends Component {
-    render() {
-        return (
-            <View style = {styles.container}>
-               <Text>This is Home page</Text>
-               <Button title = 'Go to Test' onPress = {() => this.props.navigation.navigate('home')}></Button>
-            </View>
-        )
-    }
-}
+export default class App extends Component {
+  onDecode() {
+    var token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWUzYWE2ZmE4YzBjNDU3ZDBiZDZmYzE0Iiwicm9sZSI6IlBhdGllbnQiLCJJc1N1YnNjcmliZWQiOnRydWUsImlhdCI6MTU4MDk5MjE1MywiZXhwIjoxNTgxMDEzNzUzfQ.wEQOyfh4VlY5sAbSY9pXYQaaIrBVrlp4j9l6n1gSXn4';
 
-class Test extends Component {
-    render() {
-        return (
-            <View style = {styles.container}>
-               <Text>This is Test component</Text>
-               <Button title = 'Go to Home' onPress = {() => this.props.navigation.navigate('test')}></Button>
-            </View>
-        )
-    }
+    var decoded = JwtDecode(token);
+    console.log(decoded);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{width: 100, height: 50}}>
+          <TouchableOpacity onPress={this.onDecode}>
+            <Text
+              style={{
+                fontSize: 15,
+                textAlign: 'center',
+                borderWidth: 2,
+                margin: 10,
+              }}>
+              Decode
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        alignItems : 'center',
-        justifyContent : 'center',
-        backgroundColor : '#ffeeee'
-    }
-})
-
-
-export default createStackNavigator ({
-    home : HomeApp,
-    test : Test
-})
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 10,
+  },
+});
